c1f8d6ff2a86f03d55eecbcf6a3669c7d5f17cb9
Ephraim Härer <Kephson@users.noreply.github.com>
2019-11-26 11:18:41 +0100

    updated version

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '3.5.2',
+  'version' => '3.5.3',
   'constraints' => 
   [
     'depends' => 
